48c43e32cc2adb617962014e4031e6f8f7fadc1b
sadegh <kainyarinasil@gmail.com>
2025-12-12 21:45:56 +0330

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,30 +1,32 @@
 #include "Manager.h"
 #include <stdexcept>
 #include <algorithm>
+#include <string>
 
 void Manager::addEvent(const Event& event) {
     auto result = events.emplace(event.getID(), event);
     if (!result.second) {
-        throw std::runtime_error("Event with this ID already exists!");
+
+        throw std::runtime_error("Error: Event with ID " + std::to_string(event.getID()) + " already exists!");
     }
 }
 
 void Manager::addParticipant(const Participant& participant) {
     auto result = participants.emplace(participant.getID(), participant);
     if (!result.second) {
-        throw std::runtime_error("Participant ID already exists!");
+        throw std::runtime_error("Error: Participant ID " + std::to_string(participant.getID()) + " already exists!");
     }
 }
 
 Event* Manager::findEvent(int eventID) {
     if (events.find(eventID) == events.end())
-        throw std::runtime_error("Event not found!");
+        throw std::runtime_error("Error: Event ID " + std::to_string(eventID) + " not found!");
     return &events.at(eventID);
 }
 
 Participant* Manager::findParticipant(int participantID) {
     if (participants.find(participantID) == participants.end())
-        throw std::runtime_error("Participant not found!");
+        throw std::runtime_error("Error: Participant ID " + std::to_string(participantID) + " not found!");
     return &participants.at(participantID);
 }
 
@@ -35,12 +37,23 @@ void Manager::registerParticipantToEvent(int participantID, int eventID) {
     if (ev->isFull())
         throw std::runtime_error("Cannot register: Event is full!");
 
+
+    const std::vector<int>& currentEvents = part->getRegisteredEvents();
+    for(int id : currentEvents) {
+        if(id == eventID) {
+             throw std::runtime_error("Participant is already registered for this event!");
+        }
+    }
+
     ev->registerParticipant(participantID);
     part->registerEvent(eventID);
 }
 
 std::vector<Event> Manager::listEventsSortedByType() const {
     std::vector<Event> sorted;
+
+    sorted.reserve(events.size());
+    
     for (const auto& pair : events)
         sorted.push_back(pair.second);
 
@@ -52,6 +65,8 @@ std::vector<Event> Manager::listEventsSortedByType() const {
 
 std::vector<Event> Manager::listEventsSortedByParticipantCount() const {
     std::vector<Event> sorted;
+    sorted.reserve(events.size());
+
     for (const auto& pair : events)
         sorted.push_back(pair.second);
 
