99107a558fefd4c8aafab608e6defb910b1a4b23
sadegh <kainyarinasil@gmail.com>
2025-12-12 18:28:31 +0330

    add Event

---
+++ Event.h
@@ -0,0 +1,28 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <string>
+#include <vector>
+
+class Event {
+private:
+    std::string eventName;
+    int eventID;
+    std::string eventType;
+    int capacity;
+    std::vector<int> registeredParticipants;
+
+public:
+    Event(std::string name, int id, std::string type, int cap);
+
+    std::string getName() const;
+    int getID() const;
+    std::string getType() const;
+    int getCapacity() const;
+    int getRegisteredCount() const;
+
+    void registerParticipant(int participantID);
+    bool isFull() const;
+};
+
+#endif
