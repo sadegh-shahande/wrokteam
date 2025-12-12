8b2da4860851ba1e170cf23297faa7ed93fece74
aref jabarouti <ajabarouti1382@gmail.com>
2025-12-12 18:51:11 +0330

    add Participant.h

---
+++ Participant.h
@@ -0,0 +1,23 @@
+#ifndef PARTICIPANT_H
+#define PARTICIPANT_H
+
+#include <string>
+#include <vector>
+
+class Participant {
+private:
+    std::string name;
+    int participantID;
+    std::vector<int> registeredEvents;
+
+public:
+    // Standard constructor
+    Participant(std::string name, int id);
+
+    std::string getName() const;
+    int getID() const;
+    void registerEvent(int eventID);
+    std::vector<int> getRegisteredEvents() const;
+};
+
+#endif
